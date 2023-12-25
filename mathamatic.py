5de81fda33b95f2701affe60b4498ae7450e2239
vishalinagathevan <126974938+vishalinagathevan@users.noreply.github.com>
2023-12-25 15:11:03 +0530

    Workflow created with python program

--- mathamatic.py
+++ mathamatic.py
@@ -5,20 +5,60 @@ define the python functions.
 
 # function call with two arguments
 def calc_addition(a, b):
+    """
+    Add two numbers.
+
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The sum of the two numbers.
+    """
     return a + b 
 # print(2,4)
 
 # function call with two arguments
 def calc_multiply(a, b):
+    """
+    Multiply two numbers.
+
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The product of the two numbers.
+    """
     return a * b    
 # print(3,3)
 
 # function call with two arguments
 def calc_substraction(a, b):
+    """
+    Subtract one number from another.
+
+    Args:
+        a (int): The first number.
+        b (int): The number to subtract.
+
+    Returns:
+        int: The result of the subtraction.
+    """
     return a - b
 # print(10,5)
 
 # function call with two arguments
 def calc_division(a, b):
+    """
+    Divide one number by another.
+
+    Args:
+        a (int): The numerator.
+        b (int): The denominator.
+
+    Returns:
+        float: The result of the division.
+    """
     return a / b
 # print(10,2)
