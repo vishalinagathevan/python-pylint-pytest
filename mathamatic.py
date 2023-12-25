b0f5e7c7a3537ea1cf24c9fb22c59a8832a7e30c
vishalinagathevan <126974938+vishalinagathevan@users.noreply.github.com>
2023-12-25 12:58:19 +0530

    Workflow created with python program

--- mathamatic.py
+++ mathamatic.py
@@ -6,15 +6,19 @@ define the python functions.
 # function call with two arguments
 def calc_addition(a, b):
     return a + b 
+print(2,4)
 
 # function call with two arguments
 def calc_multiply(a, b):
     return a * b    
+print(3,3)
 
 # function call with two arguments
 def calc_substraction(a, b):
     return a - b
+print(10,5)
 
 # function call with two arguments
 def calc_division(a, b):
     return a / b
+print(10,2)
